f36c6c9b44ee4f740e5c73b6568a190d0a76b885
伟love铃 <348156325@qq.com>
2018-07-29 08:34:42 +0000

    user upload avatar

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Handlers\ImageUploadHandler;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\UserRequest;
@@ -18,9 +19,16 @@ class UsersController extends Controller
         return view('users.edit',compact('user'));
     }
 
-    public function update(UserRequest $request,User $user)
+    public function update(UserRequest $request,ImageUploadHandler $upload,User $user)
     {
-        $user->update($request->all());
+        $data = $request->all();
+        if($user->avatar){
+            $result = $upload->save($request->avatar,'avatar',$user->id,362);
+            if($result){
+                $data['avatar'] = $result['path'];
+            }
+        }
+        $user->update($data);
         return redirect()->route('users.show',$user->id)->with('success','个人资料更新成功');
     }
 
