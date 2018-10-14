239242e431e7c1d771b8445002d8b68e74583369
weifeng <348156325@qq.com>
2018-10-14 11:00:31 +0000

    安装后台权限管理包和执行

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -30,8 +30,7 @@ class UserTableSeeder extends Seeder
             ->times(10)
             ->make()
             ->each(function ($user, $index)
-            use ($faker, $avatars)
-            {
+            use ($faker, $avatars) {
                 // 从头像数组中随机取出一个并赋值
                 $user->avatar = $faker->randomElement($avatars);
             });
@@ -45,9 +44,16 @@ class UserTableSeeder extends Seeder
         // 单独处理第一个用户的数据
         $user = User::find(1);
         $user->name = 'Summer';
-        $user->email = 'summer@yousails.com';
+        $user->email = '50098161@qq.com';
         $user->avatar = 'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200';
         $user->save();
+
+        // 初始化用户角色，将 1 号用户指派为『站长』
+        $user->assignRole('Founder');
+
+        // 将 2 号用户指派为『管理员』
+        $user = User::find(2);
+        $user->assignRole('Maintainer');
 
     }
 }
