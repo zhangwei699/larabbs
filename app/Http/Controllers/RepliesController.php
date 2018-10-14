833a97ed22f8464d5584fda6e4e3d1ca33706c02
weifeng <348156325@qq.com>
2018-10-14 09:52:16 +0000

    用户删除帖子和评论增加

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -15,21 +15,21 @@ class RepliesController extends Controller
         $this->middleware('auth');
     }
 
-	public function store(ReplyRequest $request,Reply $reply)
-	{
+    public function store(ReplyRequest $request, Reply $reply)
+    {
         $reply->content = $request->content;
         $reply->user_id = Auth::id();
         $reply->topic_id = $request->topic_id;
         $reply->save();
 
         return redirect()->to($reply->topic->link())->with('success', '创建成功！');
-	}
+    }
 
-	public function destroy(Reply $reply)
-	{
-		$this->authorize('destroy', $reply);
-		$reply->delete();
+    public function destroy(Reply $reply)
+    {
+        $this->authorize('destroy', $reply);
+        $reply->delete();
 
-		return redirect()->route('replies.index')->with('success', '删除成功');
-	}
+        return redirect()->to($reply->topic->link())->with('success', '成功删除回复');
+    }
 }
