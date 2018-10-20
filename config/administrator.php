6477d33b896b056efaeb1dd873c3fc75c1832e60
weifeng <348156325@qq.com>
2018-10-20 13:41:18 +0000

    增加内容管理和站点设置权限

--- config/administrator.php
+++ config/administrator.php
@@ -39,6 +39,16 @@ return array(
     'menu' => [
         '用户与权限' => [
             'users',
+            'roles',
+            'permissions',
+        ],
+        '内容管理' => [
+            'categories',
+            'topics',
+            'replies',
+        ],
+        '站点管理' => [
+            'settings.site',
         ],
     ],
 
